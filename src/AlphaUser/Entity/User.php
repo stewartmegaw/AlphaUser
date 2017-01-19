2aba61cf9fc6b8769f8de86ee645cdb38b417a4d
HARISMEHMOOD <haris.mehmood@outlook.com>
2017-01-19 10:48:43 +0400

    u

--- src/AlphaUser/Entity/User.php
+++ src/AlphaUser/Entity/User.php
@@ -1,22 +1,12 @@
 <?php
 
-/**
- * BjyAuthorize Module (https://github.com/bjyoungblood/BjyAuthorize)
- *
- * @link https://github.com/bjyoungblood/BjyAuthorize for the canonical source repository
- * @license http://framework.zend.com/license/new-bsd New BSD License
- */
-
 namespace AlphaUser\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
 use AlphaUserBase\Entity\AlphaUserBase;
 
 /**
- * An example of how to implement a role aware user entity.
- *
  * @ORM\Entity
- *
- * @author Tom Oram <tom@scl.co.uk>
  */
 class User extends AlphaUserBase {
 
